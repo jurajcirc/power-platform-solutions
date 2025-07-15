6f33a11d10bde4377f4d31964030a886a062fa29
Juraj <juraj@powerux.cz>
2025-07-15 09:04:21 +0200

    Update DiscrepancyManagement solution: 2025-07-15 09:04:21

--- update-solution.ps1
+++ update-solution.ps1
@@ -6,21 +6,29 @@ $solutionName = "DiscrepancyManagement"
 $zipFile = "./solution.zip"
 $unpackFolder = "./src/Solution"
 $gitBranch = "master"
+$logFile = "./update-log.txt"
 
 # Remove old zip if exists
 if (Test-Path $zipFile) {
     Remove-Item $zipFile
 }
 
+# Pull latest from remote to avoid conflicts
 Write-Host "Pulling latest from GitHub branch '$gitBranch'..."
 git pull --rebase origin $gitBranch
 
+# Export solution from Power Platform
 Write-Host "Exporting solution '$solutionName'..."
 pac solution export --path $zipFile --name $solutionName --managed false
 
+# Unpack solution into source structure
 Write-Host "Unpacking solution to '$unpackFolder'..."
 pac solution unpack --zipfile $zipFile --folder $unpackFolder --packagetype Unmanaged
 
+# Clean up any old unnecessary unpack files if needed
+Get-ChildItem -Recurse $unpackFolder -Include "*.bak","*.tmp" | Remove-Item -Force -ErrorAction SilentlyContinue
+
+# Git add, commit, push
 Write-Host "Adding changes to Git..."
 git add .
 
@@ -33,4 +41,8 @@ git commit -m "$commitMessage"
 Write-Host "Pushing to branch '$gitBranch'..."
 git push -u origin $gitBranch
 
-Write-Host "Done."
+# Log this update
+$lastCommit = git rev-parse HEAD
+"[$timeStamp] Updated to commit $lastCommit" | Out-File -Append -Encoding utf8 $logFile
+
+Write-Host "Done. Latest commit: $lastCommit"
