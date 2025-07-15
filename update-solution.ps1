f11e0fe4d8bd77568f397a5c0a97a098f4b4d287
Juraj <juraj@powerux.cz>
2025-07-15 08:47:33 +0200

    Clean up repo: removed nested repo and added .gitignore

--- update-solution.ps1
+++ update-solution.ps1
@@ -1,6 +1,33 @@
-pac solution export --path ./solution.zip --name DiscrepancyManagement --managed false
-pac solution unpack --zipfile ./solution.zip --folder ./src/Solution --packagetype Unmanaged
+# Export Power Platform solution and push to GitHub
+# Solution: DiscrepancyManagement
+# Branch: master
 
+$solutionName = "DiscrepancyManagement"
+$zipFile = "./solution.zip"
+$unpackFolder = "./src/Solution"
+$gitBranch = "master"
+
+# Remove old zip if exists
+if (Test-Path $zipFile) {
+    Remove-Item $zipFile
+}
+
+Write-Host "Exporting solution '$solutionName'..."
+pac solution export --path $zipFile --name $solutionName --managed false
+
+Write-Host "Unpacking solution to '$unpackFolder'..."
+pac solution unpack --zipfile $zipFile --folder $unpackFolder --packagetype Unmanaged
+
+Write-Host "Adding changes to Git..."
 git add .
-git commit -m "Update solution"
-git push
+
+$timeStamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+$commitMessage = "Update DiscrepancyManagement solution: $timeStamp"
+
+Write-Host "Committing changes..."
+git commit -m "$commitMessage"
+
+Write-Host "Pushing to branch '$gitBranch'..."
+git push -u origin $gitBranch
+
+Write-Host "Done."
